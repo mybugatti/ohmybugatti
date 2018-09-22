c2d418cf1bb1a4dcbdcd655863ac893407ea7e82
Bugatti <gradignan.bx@gmail.com>
2018-09-22 21:21:18 +0200

    add new component

---
+++ src/app/hr/hr.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-hr',
+  templateUrl: './hr.component.html',
+  styleUrls: ['./hr.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class HrComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

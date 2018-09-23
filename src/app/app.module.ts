c2b319f02b6dbed340511e5cd575fd489bf65501
Bugatti <gradignan.bx@gmail.com>
2018-09-23 20:26:37 +0200

    add fr pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HrComponent } from './hr/hr.component';
 import { TestComponent } from './test/test.component';
+import { FrPipe } from './fr.pipe';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HrComponent,
-    TestComponent
+    TestComponent,
+    FrPipe
   ],
   imports: [
     BrowserModule
---
+++ src/app/fr.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FrPipe } from './fr.pipe';
+
+describe('FrPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FrPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
